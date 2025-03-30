196fd2fa418702e6c4972af707d5c08bf9035f5c
Brian Norman Peña Calero <brianmsm@gmail.com>
2025-03-30 18:45:02 +0200

    num de opciones validas modificadas

--- funciones/04_procesar_decision.R
+++ funciones/04_procesar_decision.R
@@ -2,16 +2,16 @@
 # Lee y valida la entrada del jugador (opciones del menú)
 
 procesar_decision <- function() {
-  opciones_validas <- 1:6
+  opciones_validas <- 1:5
   
   repeat {
-    opcion <- readline(prompt = "Selecciona una opción (1-6): ")
+    opcion <- readline(prompt = "Selecciona una opción (1-5): ")
     
     # Validar si es número y está dentro del rango
-    if (grepl("^[1-6]$", opcion)) {
+    if (grepl("^[1-5]$", opcion)) {
       return(as.integer(opcion))
     } else {
-      cat("❌ Entrada inválida. Por favor elige un número entre 1 y 6.\n")
+      cat("❌ Entrada inválida. Por favor elige un número entre 1 y 5.\n")
     }
   }
 }
