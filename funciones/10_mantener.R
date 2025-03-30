001e0d24cf0ae7287a626929281e24d8ef21e0d9
Brian Norman Peña Calero <brianmsm@gmail.com>
2025-03-30 18:19:02 +0200

    Mantener acciones

---
+++ funciones/10_mantener.R
@@ -0,0 +1,7 @@
+# ===== mantener() =====
+# El jugador decide no realizar ninguna acción
+
+mantener <- function(estado) {
+  cat("\n⏸️ Has decidido mantener tus inversiones sin cambios por hoy.\n")
+  return(estado)
+}
